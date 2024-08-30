7c52dca1d467179e00b2e802af214f0639aa7288
Soomin <gil2093ab@gmail.com>
2024-08-30 17:00:54 +0900

    init: dotenv string variable

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,4 +1,5 @@
 import 'package:logger/logger.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final Logger logger = Logger(
   printer: PrettyPrinter(
@@ -9,3 +10,7 @@ final Logger logger = Logger(
     printEmojis: true,
   ),
 );
+
+final String apiUrl = dotenv.env['API_URL']!;
+final String supabaseUrl = dotenv.env['SUPABASE_URL']!;
+final String supabaseKey = dotenv.env['SUPABASE_KEY']!;
