99c9c716b2f7a548ac30500260682a78b0df9fde
Soomin <gil2093ab@gmail.com>
2024-09-01 14:48:23 +0900

    feat: auth cubit token logic

--- lib/cubit/auth_cubit.dart
+++ lib/cubit/auth_cubit.dart
@@ -6,15 +6,21 @@ part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial()) {
-    _anonymousLogin();
+    final currentUser = supabase.auth.currentUser;
+    if (currentUser != null) {
+      emit(Authenticated(currentUser.id));
+    } else {
+      _anonymousLogin();
+    }
   }
 
   Future<void> _anonymousLogin() async {
     try {
-      await supabase.auth.signInAnonymously().then((value) {
-        logger.d(value);
-      });
-      emit(Authenticated("Anonymous"));
+      final response = await supabase.auth.signInAnonymously();
+      if (response.user == null) {
+        throw Exception('Failed to sign in');
+      }
+      emit(Authenticated(response.user!.id));
     } catch (e) {
       logger.e(e.toString());
       emit(AuthError());
