da418ec32bf38a9e8aa92e2f88b698c69a6777a0
Soomin <gil2093ab@gmail.com>
2024-09-03 15:15:54 +0900

    feat: add fab button to home view

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:udaadaa/widgets/fab.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Home');
+    return const Scaffold(
+      body: Text('Home'),
+      floatingActionButton: AddFabButton(),
+    );
   }
 }
