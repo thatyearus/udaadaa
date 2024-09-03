f04bcceb0ca054a06c99e02f07a95c21ef8035ed
Soomin <gil2093ab@gmail.com>
2024-09-03 15:20:45 +0900

    feat: add fab button to mypage view

--- lib/view/mypage/mypage_view.dart
+++ lib/view/mypage/mypage_view.dart
@@ -3,39 +3,43 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:udaadaa/cubit/auth_cubit.dart';
 import 'package:udaadaa/cubit/profile_cubit.dart';
 import 'package:udaadaa/utils/constant.dart';
+import 'package:udaadaa/widgets/fab.dart';
 
 class MyPageView extends StatelessWidget {
   const MyPageView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ProfileCubit, ProfileState>(
-      builder: (context, state) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('My Page'),
-            AppSpacing.verticalSizedBoxS,
-            if (state is ProfileLoaded)
-              Row(
-                children: [
-                  const CircleAvatar(
-                    radius: 30,
-                    child: Icon(Icons.person),
-                  ),
-                  AppSpacing.horizontalSizedBoxS,
-                  Text(state.user),
-                ],
+    return Scaffold(
+      body: BlocBuilder<ProfileCubit, ProfileState>(
+        builder: (context, state) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('My Page'),
+              AppSpacing.verticalSizedBoxS,
+              if (state is ProfileLoaded)
+                Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 30,
+                      child: Icon(Icons.person),
+                    ),
+                    AppSpacing.horizontalSizedBoxS,
+                    Text(state.user),
+                  ],
+                ),
+              ElevatedButton(
+                onPressed: () {
+                  context.read<AuthCubit>().signOut();
+                },
+                child: const Text('Sign Out'),
               ),
-            ElevatedButton(
-              onPressed: () {
-                context.read<AuthCubit>().signOut();
-              },
-              child: const Text('Sign Out'),
-            ),
-          ],
-        );
-      },
+            ],
+          );
+        },
+      ),
+      floatingActionButton: const AddFabButton(),
     );
   }
 }
