43fad6d6b21007b642f0bf0c71fc6e34465a7339
Soomin <gil2093ab@gmail.com>
2024-09-03 17:27:46 +0900

    feat: set my_record view navigation for mypage view

--- lib/view/mypage/mypage_view.dart
+++ lib/view/mypage/mypage_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:udaadaa/cubit/auth_cubit.dart';
 import 'package:udaadaa/cubit/profile_cubit.dart';
 import 'package:udaadaa/utils/constant.dart';
+import 'package:udaadaa/view/detail/my_record_view.dart';
 import 'package:udaadaa/widgets/fab.dart';
 
 class MyPageView extends StatelessWidget {
@@ -34,6 +35,33 @@ class MyPageView extends StatelessWidget {
                   context.read<AuthCubit>().signOut();
                 },
                 child: const Text('Sign Out'),
+              ),
+              Expanded(
+                child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      crossAxisSpacing: 4.0,
+                      mainAxisSpacing: 4.0,
+                      childAspectRatio: 1.0,
+                    ),
+                    itemCount: 9,
+                    itemBuilder: (context, index) {
+                      return GridTile(
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => const MyRecordView()));
+                          },
+                          child: Container(
+                            color: AppColors.neutral[100],
+                            child: Center(
+                              child: Text('Item $index'),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
               ),
             ],
           );
