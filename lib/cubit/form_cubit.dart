b9f11908131c1a1716757e0e051fd72e92300b3f
Soomin <gil2093ab@gmail.com>
2024-09-05 01:47:59 +0900

    feat: image update logic

--- lib/cubit/form_cubit.dart
+++ lib/cubit/form_cubit.dart
@@ -1,8 +1,26 @@
 import 'package:bloc/bloc.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:meta/meta.dart';
 
 part 'form_state.dart';
 
 class FormCubit extends Cubit<FormState> {
   FormCubit() : super(FormInitial());
+
+  final Map<String, XFile?> _selectedImages = {
+    'FOOD': null,
+    'EXERCISE': null,
+    'WEIGHT': null,
+  };
+
+  Future<void> updateImage(String type) async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? pickedFile =
+        await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      _selectedImages[type] = pickedFile;
+      emit(FormInitial());
+    }
+  }
 }
