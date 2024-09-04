a7501fd75415d5d5526b90573f960869abc7d3d1
Soomin <gil2093ab@gmail.com>
2024-09-05 02:01:20 +0900

    feat: upload image logic

--- lib/cubit/form_cubit.dart
+++ lib/cubit/form_cubit.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
+import 'package:image/image.dart' as img;
 import 'package:image_picker/image_picker.dart';
 import 'package:meta/meta.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:udaadaa/utils/constant.dart';
 
 part 'form_state.dart';
 
@@ -21,6 +26,54 @@ class FormCubit extends Cubit<FormState> {
     if (pickedFile != null) {
       _selectedImages[type] = pickedFile;
       emit(FormInitial());
+    }
+  }
+
+  Future<File?> compressImage(File file) async {
+    try {
+      final img.Image? image = img.decodeImage(file.readAsBytesSync());
+
+      if (image == null) {
+        throw Exception('Unable to decode image');
+      }
+
+      img.Image resizedImage = img.copyResize(image, width: 1024);
+
+      final Directory tempDir = await getTemporaryDirectory();
+      final String tempPath = tempDir.path;
+      final File compressedImage =
+          File('$tempPath/${DateTime.now().microsecondsSinceEpoch}.jpg')
+            ..writeAsBytesSync(img.encodeJpg(resizedImage, quality: 70));
+
+      return compressedImage;
+    } catch (e) {
+      logger.e(e);
+      return null;
+    }
+  }
+
+  Future<void> uploadImage(String type) async {
+    final XFile? file = _selectedImages[type];
+    if (file == null) {
+      emit(FormError('No image selected'));
+      return;
+    }
+
+    try {
+      emit(FormLoading());
+      final File? compressedImage = await compressImage(File(file.path));
+      if (compressedImage == null) {
+        emit(FormError('Failed to compress image'));
+        return;
+      }
+      final userId = supabase.auth.currentUser?.id;
+      final imagePath =
+          '$userId/$type/${DateTime.now().microsecondsSinceEpoch}.jpg';
+      await supabase.storage
+          .from('FeedImages')
+          .upload(imagePath, compressedImage);
+    } catch (e) {
+      emit(FormError(e.toString()));
     }
   }
 }
