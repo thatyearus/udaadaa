49ed45752005a3407df5f4d4e2f729ae69ec6019
Soomin <gil2093ab@gmail.com>
2024-09-06 19:15:11 +0900

    refactor: erase blocbuilder and calculate at first

--- lib/widgets/report_summary.dart
+++ lib/widgets/report_summary.dart
@@ -9,26 +9,26 @@ class ReportSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final report = context.watch<ProfileCubit>().getReport;
+    final totalCalorie = (report != null
+        ? ((report.breakfast ?? 0) +
+            (report.lunch ?? 0) +
+            (report.dinner ?? 0) +
+            (report.snack ?? 0))
+        : 0);
+    final totalExercise = report?.exercise ?? 0;
+    final weight = report?.weight ?? 0;
+
     return Container(
       color: AppColors.neutral[100],
       padding: AppSpacing.edgeInsetsM,
       width: double.infinity,
       alignment: Alignment.center,
-      child: BlocBuilder<ProfileCubit, ProfileState>(
-        builder: (BuildContext context, ProfileState state) {
-          if (report != null) {
-            return Column(children: [
-              const Text("오늘의 기록"),
-              Text(
-                  "총 칼로리 : ${(report.breakfast ?? 0) + (report.lunch ?? 0) + (report.dinner ?? 0) + (report.snack ?? 0)} kcal"),
-              Text("운동 시간 : ${report.exercise ?? 0} 분"),
-              Text("체중 : ${report.weight ?? 0} kg"),
-            ]);
-          } else {
-            return const Text("오늘의 기록이 없습니다.");
-          }
-        },
-      ),
+      child: Column(children: [
+        const Text("오늘의 기록"),
+        Text("총 칼로리 : $totalCalorie kcal"),
+        Text("운동 시간 : $totalExercise 분"),
+        Text("체중 : $weight kg"),
+      ]),
     );
   }
 }
