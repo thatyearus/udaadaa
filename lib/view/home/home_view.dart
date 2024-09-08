c7641eb82ef4e3e531656b662c129a8ac088e9cc
Soomin <gil2093ab@gmail.com>
2024-09-08 14:38:37 +0900

    fix: SingleChildScrollView to home_view for scrolling

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -18,64 +18,72 @@ class HomeView extends StatelessWidget {
         onRefresh: () {
           return context.read<ProfileCubit>().getMyTodayReport();
         },
-        child: Column(children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const MyRecordView(),
-                ),
-              );
-            },
-            child: Container(
-              color: AppColors.neutral[100],
-              padding: AppSpacing.edgeInsetsM,
-              width: double.infinity,
-              alignment: Alignment.center,
-              child: const Text("내 최근 기록"),
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: Column(children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MyRecordView(),
+                  ),
+                );
+              },
+              child: Container(
+                color: AppColors.neutral[100],
+                padding: AppSpacing.edgeInsetsM,
+                width: double.infinity,
+                alignment: Alignment.center,
+                child: const Text("내 최근 기록"),
+              ),
             ),
-          ),
-          AppSpacing.verticalSizedBoxXl,
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ReportView(),
-                ),
-              );
-            },
-            child: const ReportSummary(),
-          ),
-          AppSpacing.verticalSizedBoxL,
-          Expanded(
-            child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 4.0,
-                  mainAxisSpacing: 4.0,
-                  childAspectRatio: 1.0,
-                ),
-                itemCount: 3,
-                itemBuilder: (context, index) {
-                  return GridTile(
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => const RecordView()));
-                      },
-                      child: Container(
-                        color: AppColors.neutral[100],
-                        child: Center(
-                          child: Text('Item $index'),
-                        ),
-                      ),
+            AppSpacing.verticalSizedBoxXl,
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ReportView(),
+                  ),
+                );
+              },
+              child: const ReportSummary(),
+            ),
+            AppSpacing.verticalSizedBoxL,
+            Row(children: [
+              Expanded(
+                child: GridView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      crossAxisSpacing: 4.0,
+                      mainAxisSpacing: 4.0,
+                      childAspectRatio: 1.0,
                     ),
-                  );
-                }),
-          ),
-        ]),
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return GridTile(
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => const RecordView()));
+                          },
+                          child: Container(
+                            color: AppColors.neutral[100],
+                            child: Center(
+                              child: Text('Item $index'),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+            ]),
+          ]),
+        ),
       ),
       floatingActionButton: const AddFabButton(),
     );
