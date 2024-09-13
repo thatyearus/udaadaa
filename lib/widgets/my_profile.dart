4cfb65f008e7838095ef92b7c29418b97dd71189
Soomin <gil2093ab@gmail.com>
2024-09-14 00:17:39 +0900

    design: card ui for profile

--- lib/widgets/my_profile.dart
+++ lib/widgets/my_profile.dart
@@ -9,15 +9,32 @@ class MyProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final profile = context.watch<AuthCubit>().getProfile;
-    return Row(
-      children: [
-        const CircleAvatar(
-          radius: 30,
-          child: Icon(Icons.person),
-        ),
-        AppSpacing.horizontalSizedBoxS,
-        Text(profile?.nickname ?? 'Not Logged In'),
-      ],
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.neutral[0],
+        border: Border.all(color: AppColors.primary[100]!),
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.primary[100]!,
+            offset: const Offset(0, 2),
+            blurRadius: 4,
+          ),
+        ],
+      ),
+      padding: AppSpacing.edgeInsetsM,
+      width: double.infinity,
+      alignment: Alignment.centerLeft,
+      child: Row(
+        children: [
+          const CircleAvatar(
+            radius: 30,
+            child: Icon(Icons.person),
+          ),
+          AppSpacing.horizontalSizedBoxS,
+          Text(profile?.nickname ?? 'Not Logged In'),
+        ],
+      ),
     );
   }
 }
