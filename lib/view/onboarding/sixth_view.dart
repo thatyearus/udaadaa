a55b3ed12279f00b25c30a61ef6aecbad7e317f2
Soomin <gil2093ab@gmail.com>
2024-09-24 17:10:50 +0900

    feat: onboarding animation

--- lib/view/onboarding/sixth_view.dart
+++ lib/view/onboarding/sixth_view.dart
@@ -69,7 +69,24 @@ class OnboardingFeedViewState extends State<OnboardingFeedView> {
     if (_pageController.position.pixels >
         _pageController.position.maxScrollExtent) {
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const MainView()),
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) =>
+              const MainView(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            const begin = Offset(0.0, 1.0); // 아래에서 위로
+            const end = Offset.zero;
+            const curve = Curves.ease;
+
+            var tween =
+                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+            var offsetAnimation = animation.drive(tween);
+
+            return SlideTransition(
+              position: offsetAnimation,
+              child: child,
+            );
+          },
+        ),
       );
     }
   }
