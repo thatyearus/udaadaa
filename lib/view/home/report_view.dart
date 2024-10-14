2a7b00a0e7083d49e83b78dab9cff7ce5a0cba1e
Soomin <gil2093ab@gmail.com>
2024-10-14 15:37:52 +0900

    feat: erase exercise and weight

--- lib/view/home/report_view.dart
+++ lib/view/home/report_view.dart
@@ -52,95 +52,6 @@ class ReportView extends StatelessWidget {
                   ],
                 ),
                 AppSpacing.verticalSizedBoxL,
-
-                /*
-              Container(
-                width: double.infinity,
-                padding: AppSpacing.edgeInsetsS,
-                decoration: BoxDecoration(
-                  color: AppColors.neutral[0],
-                  border: Border.all(color: AppColors.primary[100]!),
-                  borderRadius: BorderRadius.circular(8),
-                  boxShadow: [
-                    BoxShadow(
-                      color: AppColors.primary[100]!,
-                      offset: const Offset(0, 2),
-                      blurRadius: 4,
-                    ),
-                  ],
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("총칼로리", style: AppTextStyles.textTheme.headlineMedium),
-                    Text("$totalCalorie kcal",
-                        style: AppTextStyles.textTheme.bodyLarge),
-                    /*
-                    Text("아침 : ${report?.breakfast ?? 0} kcal"),
-                    Text("점심 : ${report?.lunch ?? 0} kcal"),
-                    Text("저녁 : ${report?.dinner ?? 0} kcal"),
-                    Text("간식 : ${report?.snack ?? 0} kcal"),*/
-                  ],
-                ),
-              ),
-              AppSpacing.verticalSizedBoxM,
-              Row(children: [
-                Expanded(
-                  child: Container(
-                    width: double.infinity,
-                    padding: AppSpacing.edgeInsetsS,
-                    decoration: BoxDecoration(
-                      color: AppColors.neutral[0],
-                      border: Border.all(color: AppColors.primary[100]!),
-                      borderRadius: BorderRadius.circular(8),
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppColors.primary[100]!,
-                          offset: const Offset(0, 2),
-                          blurRadius: 4,
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("운동 시간",
-                            style: AppTextStyles.textTheme.headlineMedium),
-                        Text("$totalExercise 분",
-                            style: AppTextStyles.textTheme.bodyLarge),
-                      ],
-                    ),
-                  ),
-                ),
-                AppSpacing.horizontalSizedBoxM,
-                Expanded(
-                  child: Container(
-                    width: double.infinity,
-                    padding: AppSpacing.edgeInsetsS,
-                    decoration: BoxDecoration(
-                      color: AppColors.neutral[0],
-                      border: Border.all(color: AppColors.primary[100]!),
-                      borderRadius: BorderRadius.circular(8),
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppColors.primary[100]!,
-                          offset: const Offset(0, 2),
-                          blurRadius: 4,
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("체중",
-                            style: AppTextStyles.textTheme.headlineMedium),
-                        Text("$weight kg",
-                            style: AppTextStyles.textTheme.bodyLarge),
-                      ],
-                    ),
-                  ),
-                ),
-              ]),*/
                 Row(
                   children: [
                     CardView(
@@ -196,34 +107,6 @@ class ReportView extends StatelessWidget {
                     ),
                   ],
                 ),
-                AppSpacing.verticalSizedBoxL,
-                Row(
-                  children: [
-                    CardView(
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("운동 시간",
-                                style: AppTextStyles.textTheme.headlineMedium),
-                            AppSpacing.verticalSizedBoxXxs,
-                            Text("${report?.exercise ?? 0} 분",
-                                style: AppTextStyles.textTheme.bodyLarge),
-                          ]),
-                    ),
-                    AppSpacing.horizontalSizedBoxXs,
-                    CardView(
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("체중",
-                                style: AppTextStyles.textTheme.headlineMedium),
-                            AppSpacing.verticalSizedBoxXxs,
-                            Text("${report?.weight ?? 0.0} kg",
-                                style: AppTextStyles.textTheme.bodyLarge),
-                          ]),
-                    ),
-                  ],
-                )
               ],
             ),
           ),
