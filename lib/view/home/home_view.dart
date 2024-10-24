34b09a6cd27178d7d6b84f436c1e7e1be577f3d7
Soomin <gil2093ab@gmail.com>
2024-10-24 21:27:06 +0900

    feat: request alarm permission at home view

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:udaadaa/cubit/auth_cubit.dart';
 import 'package:udaadaa/cubit/feed_cubit.dart';
 import 'package:udaadaa/cubit/profile_cubit.dart';
 import 'package:udaadaa/utils/constant.dart';
@@ -25,6 +26,7 @@ class HomeViewState extends State<HomeView> {
   void initState() {
     super.initState();
     _pageController = PageController();
+    context.read<AuthCubit>().setFCMToken();
   }
 
   @override
