82ee976841509a6ad34ff57eb17a8dd44936d653
Soomin <gil2093ab@gmail.com>
2024-10-26 15:49:55 +0900

    refactor: separate home view

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -21,78 +21,175 @@ class HomeView extends StatefulWidget {
 
 class HomeViewState extends State<HomeView> {
   late PageController _pageController;
+  late PageController _sectionController;
+
+  int _selectedIndex = 0;
+
+  void _onButtonTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   void initState() {
     super.initState();
     _pageController = PageController();
+    _sectionController = PageController();
     context.read<AuthCubit>().setFCMToken();
   }
 
   @override
   void dispose() {
     _pageController.dispose();
+    _sectionController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final myFeedsLength =
-        context.select<FeedCubit, int>((cubit) => cubit.getMyFeeds.length);
     return Scaffold(
-      body: RefreshIndicator(
-        onRefresh: () {
-          return Future.wait([
-            context.read<FeedCubit>().fetchMyFeeds(),
-            //context.read<FeedCubit>().fetchHomeFeeds(),
-            context.read<ProfileCubit>().getMyTodayReport(),
-          ]);
-        },
-        child: SingleChildScrollView(
-          padding: AppSpacing.edgeInsetsL,
-          physics: const AlwaysScrollableScrollPhysics(),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 250,
-                child: PageView.builder(
-                  controller: _pageController,
-                  itemCount: min(3, myFeedsLength),
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Analytics().logEvent(
-                          "홈_최근기록",
-                          parameters: {"최근기록_페이지": (index + 1).toString()},
-                        );
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                MyRecordView(initialPage: index),
-                          ),
-                        );
-                      },
-                      child: LastRecord(page: index),
-                    );
-                  },
+      body: Column(
+        children: [
+          SelectButton(
+            selectedIndex: _selectedIndex,
+            onSelect: _onButtonTapped,
+          ),
+          Expanded(
+            child: _selectedIndex == 0
+                ? const ChallengeHomeView()
+                : ReportHomeView(pageController: _pageController),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SelectButton extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onSelect;
+
+  const SelectButton({
+    super.key,
+    required this.selectedIndex,
+    required this.onSelect,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Divider(color: AppColors.neutral[300]),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton(
+              onPressed: () => onSelect(0),
+              child: Text(
+                '챌린지',
+                style: TextStyle(
+                  color: selectedIndex == 0
+                      ? AppColors.primary
+                      : AppColors.neutral[500],
+                  fontWeight:
+                      selectedIndex == 0 ? FontWeight.bold : FontWeight.normal,
                 ),
               ),
-              AppSpacing.verticalSizedBoxL,
-              GestureDetector(
-                child: const ReportSummary(),
-                onTap: () {
-                  Analytics().logEvent(
-                    "홈_리포트",
-                  );
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const ReportView()),
+            ),
+            TextButton(
+              onPressed: () => onSelect(1),
+              child: Text(
+                '리포트',
+                style: TextStyle(
+                  color: selectedIndex == 1
+                      ? AppColors.primary
+                      : AppColors.neutral[500],
+                  fontWeight:
+                      selectedIndex == 1 ? FontWeight.bold : FontWeight.normal,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Divider(color: AppColors.neutral[300]),
+      ],
+    );
+  }
+}
+
+class ChallengeHomeView extends StatelessWidget {
+  const ChallengeHomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('ChallengeHomeView'),
+    );
+  }
+}
+
+class ReportHomeView extends StatelessWidget {
+  final PageController _pageController;
+  const ReportHomeView({super.key, required PageController pageController})
+      : _pageController = pageController;
+
+  @override
+  Widget build(BuildContext context) {
+    final myFeedsLength =
+        context.select<FeedCubit, int>((cubit) => cubit.getMyFeeds.length);
+    return RefreshIndicator(
+      onRefresh: () {
+        return Future.wait([
+          context.read<FeedCubit>().fetchMyFeeds(),
+          //context.read<FeedCubit>().fetchHomeFeeds(),
+          context.read<ProfileCubit>().getMyTodayReport(),
+        ]);
+      },
+      child: SingleChildScrollView(
+        padding: AppSpacing.edgeInsetsL,
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 250,
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: min(3, myFeedsLength),
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Analytics().logEvent(
+                        "홈_최근기록",
+                        parameters: {"최근기록_페이지": (index + 1).toString()},
+                      );
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              MyRecordView(initialPage: index),
+                        ),
+                      );
+                    },
+                    child: LastRecord(page: index),
                   );
                 },
-              )
-            ],
-          ),
+              ),
+            ),
+            AppSpacing.verticalSizedBoxL,
+            GestureDetector(
+              child: const ReportSummary(),
+              onTap: () {
+                Analytics().logEvent(
+                  "홈_리포트",
+                );
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ReportView()),
+                );
+              },
+            )
+          ],
         ),
       ),
     );
