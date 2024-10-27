b1006bf92748b851c3ee2694919ab51601aaa427
Soomin <gil2093ab@gmail.com>
2024-10-27 16:01:53 +0900

    feat: init notification service

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:udaadaa/firebase_options.dart';
 import 'package:udaadaa/utils/constant.dart';
 import 'package:udaadaa/service/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:udaadaa/utils/notifications/notification_service.dart';
 import 'package:udaadaa/view/splash_view.dart';
 import 'package:udaadaa/utils/analytics/analytics.dart';
 import 'package:facebook_app_events/facebook_app_events.dart';
@@ -49,6 +50,7 @@ Future<void> main() async {
       options: DefaultFirebaseOptions.currentPlatform,
     ),
     initializeDateFormatting(),
+    NotificationService.initNotification(),
   ]);
   Analytics().init();
 
