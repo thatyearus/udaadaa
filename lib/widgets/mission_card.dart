633415bcb8309232d2d72267d2876af15ab98578
Soomin <gil2093ab@gmail.com>
2024-10-30 02:52:36 +0900

    feat: initial mission card

---
+++ lib/widgets/mission_card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:udaadaa/utils/constant.dart';
+
+class MissionCard extends StatelessWidget {
+  const MissionCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: AppSpacing.edgeInsetsM,
+      margin: const EdgeInsets.symmetric(
+        vertical: AppSpacing.xxs,
+      ),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: AppColors.neutral[50],
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "미션",
+            style: AppTextStyles.textTheme.headlineMedium,
+          ),
+          Text(
+            "미션을 수행하세요!",
+            style: AppTextStyles.textTheme.bodyLarge,
+          ),
+        ],
+      ),
+    );
+  }
+}
