5b5d6d4052a403f7287eef9f6b56968ae71d5ccc
Soomin <gil2093ab@gmail.com>
2024-12-20 21:17:09 +0900

    feat: initial chat cubit setting

---
+++ lib/cubit/chat_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+// import 'package:udaadaa/models/room.dart';
+// import 'package:udaadaa/utils/constant.dart';
+
+part 'chat_state.dart';
+
+class ChatCubit extends Cubit<ChatState> {
+  // List<Room> chatList = [];
+
+  ChatCubit() : super(ChatInitial()) {
+    loadChatList();
+  }
+
+  Future<void> loadChatList() async {
+    // final ret = await supabase.from('rooms').select();
+    // logger.d("loadChatList: $ret");
+    // chatList = ret.map((e) => Room.fromMap(e)).toList();
+    // emit(ChatListLoaded());
+  }
+
+  // List<Room> get getChatList => chatList;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:udaadaa/cubit/auth_cubit.dart';
 import 'package:udaadaa/cubit/bottom_nav_cubit.dart';
 import 'package:udaadaa/cubit/challenge_cubit.dart';
+import 'package:udaadaa/cubit/chat_cubit.dart';
 import 'package:udaadaa/cubit/feed_cubit.dart';
 import 'package:udaadaa/cubit/form_cubit.dart';
 import 'package:udaadaa/cubit/profile_cubit.dart';
@@ -89,6 +90,9 @@ class MyApp extends StatelessWidget {
             context.read<ProfileCubit>(),
             context.read<FeedCubit>(),
           ),
+        ),
+        BlocProvider<ChatCubit>(
+          create: (context) => ChatCubit(),
         ),
       ],
       child: MaterialApp(
