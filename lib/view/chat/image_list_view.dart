a21d1e0c7698e0328b4ed75134ebe883e6ff621c
Soomin <gil2093ab@gmail.com>
2025-01-14 12:11:15 +0900

    feat: add navigation to ImageDetailView from ImageListView for viewing image details

--- lib/view/chat/image_list_view.dart
+++ lib/view/chat/image_list_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:udaadaa/models/message.dart';
 import 'package:udaadaa/models/room.dart';
 import 'package:udaadaa/utils/constant.dart';
+import 'package:udaadaa/view/chat/image_detail_view.dart';
 
 class ImageListView extends StatelessWidget {
   const ImageListView(
@@ -31,6 +32,16 @@ class ImageListView extends StatelessWidget {
         itemCount: imageMessages.length,
         itemBuilder: (context, index) {
           return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => ImageDetailView(
+                    imageMessage: imageMessages[index],
+                    roomInfo: roomInfo,
+                  ),
+                ),
+              );
+            },
             child: CachedNetworkImage(
               imageUrl: imageMessages[index].imageUrl!,
               fit: BoxFit.cover,
