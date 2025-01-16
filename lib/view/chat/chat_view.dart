d75e6197337fc9f1ad8891818a1789e41bf969bd
Soomin <gil2093ab@gmail.com>
2025-01-16 20:22:44 +0900

    feat: add navigation to ExerciseFirstView in chat view

--- lib/view/chat/chat_view.dart
+++ lib/view/chat/chat_view.dart
@@ -14,6 +14,7 @@ import 'package:udaadaa/models/room.dart';
 import 'package:udaadaa/utils/constant.dart';
 import 'package:udaadaa/view/chat/image_list_view.dart';
 import 'package:udaadaa/view/chat/profile_view.dart';
+import 'package:udaadaa/view/form/exercise/exercise_first_view.dart';
 import 'package:udaadaa/view/form/weight/weight_first_view.dart';
 import 'package:udaadaa/view/onboarding/first_view.dart';
 import 'package:udaadaa/widgets/chat_bubble.dart';
@@ -279,7 +280,13 @@ class ChatView extends StatelessWidget {
                                 builder: (context) => const WeightFirstView(),
                               ),
                             );
-                          } else {}
+                          } else {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => const ExerciseFirstView(),
+                              ),
+                            );
+                          }
                         },
                       ),
                       AppSpacing.verticalSizedBoxXs,
