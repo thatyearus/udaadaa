47719f7885648b788e74396e36f3bc6183ee059f
Soomin Kil <71880464+2093ab@users.noreply.github.com>
2025-01-17 22:20:48 +0900

    supabase: remove unused function

---
+++ supabase/migrations/20250117130707_remove_unused_function.sql
@@ -0,0 +1,3 @@
+drop function if exists "public"."mission_complete"(user_id uuid, review text, feed_type "FeedType", feed_image_path text, calorie integer, room_id uuid, content text, message_image_path text, message_type "MessageType");
+
+
