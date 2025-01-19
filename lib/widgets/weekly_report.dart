adff2940c8d7c5a2a199aa6a84a41fe159d5d85b
Soomin <gil2093ab@gmail.com>
2025-01-19 12:01:26 +0900

    feat: add pagination controls to WeeklyReport widget

--- lib/widgets/weekly_report.dart
+++ lib/widgets/weekly_report.dart
@@ -1,4 +1,6 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:udaadaa/utils/constant.dart';
 import 'package:udaadaa/widgets/calorie_report.dart';
 import 'package:udaadaa/widgets/exercise_report.dart';
 import 'package:udaadaa/widgets/weight_report.dart';
@@ -11,6 +13,47 @@ class WeeklyReport extends StatefulWidget {
 }
 
 class _WeeklyReportState extends State<WeeklyReport> {
+  late PageController _pageController;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void previousPage() {
+    if (_selectedIndex > 0) {
+      _pageController.animateToPage(
+        _selectedIndex - 1,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+      setState(() {
+        _selectedIndex--;
+      });
+    }
+  }
+
+  void nextPage() {
+    if (_selectedIndex < 2) {
+      _pageController.animateToPage(
+        _selectedIndex + 1,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+      setState(() {
+        _selectedIndex++;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     List<Widget> pages = [
@@ -19,10 +62,42 @@ class _WeeklyReportState extends State<WeeklyReport> {
       const ExerciseReport()
     ];
     return SizedBox(
-      height: 400,
-      child: PageView.builder(
-        itemCount: 3,
-        itemBuilder: (context, index) => pages[index],
+      height: 350,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Expanded(
+            child: PageView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) => pages[index],
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: _selectedIndex > 0 ? previousPage : null,
+                icon: const Icon(FluentIcons.chevron_left_12_regular),
+                disabledColor: AppColors.neutral[300],
+                color: AppColors.neutral[700],
+              ),
+              Expanded(
+                  child: Text(
+                "${_selectedIndex + 1} / 3 페이지",
+                textAlign: TextAlign.center,
+                style: AppTextStyles.textTheme.bodyMedium,
+              )),
+              IconButton(
+                  onPressed: _selectedIndex < 2 ? nextPage : null,
+                  icon: const Icon(FluentIcons.chevron_right_12_regular)),
+            ],
+          ),
+        ],
       ),
     );
   }
