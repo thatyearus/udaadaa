93c7500d12146534b9e505c116112f478d20a6af
Soomin <gil2093ab@gmail.com>
2025-02-15 23:39:01 +0900

    fix: remove unused import in category.dart

--- lib/widgets/category.dart
+++ lib/widgets/category.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:udaadaa/cubit/feed_cubit.dart';
-import 'package:udaadaa/utils/constant.dart';
 
 class CategoryButtonsContainer extends StatefulWidget {
   final ValueChanged<FeedCategory> onCategorySelected;
