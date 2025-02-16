e91ffe3901e5ed226a3c44a9f5825bccf3b005a1
Soomin <gil2093ab@gmail.com>
2025-02-16 21:12:48 +0900

    fix: update BoxShadow color opacity method in fifth_view.dart to use withValues

--- lib/view/onboarding/fifth_view.dart
+++ lib/view/onboarding/fifth_view.dart
@@ -138,7 +138,7 @@ class FifthView extends StatelessWidget {
             color: Colors.white,
             boxShadow: [
               BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
+                color: Colors.grey.withValues(alpha: 0.5),
                 spreadRadius: 1,
                 blurRadius: 10,
                 offset: const Offset(0, 3),
