21336fff3880680d9391ee18816dca2eecdf886f
Soomin <gil2093ab@gmail.com>
2025-02-18 12:44:02 +0900

    fix: update loading animation asset to use pink variant

--- lib/view/form/exercise/exercise_second_view.dart
+++ lib/view/form/exercise/exercise_second_view.dart
@@ -59,8 +59,8 @@ class _ExerciseSecondViewState extends State<ExerciseSecondView> {
           builder: (context, state) {
             if (state is form.FormLoading) {
               return Center(
-                child:
-                    Lottie.asset('assets/loading_animation.json', width: 150),
+                child: Lottie.asset('assets/loading_pink_animation.json',
+                    width: 150),
               );
             }
 
