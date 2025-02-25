1e7f992f71eb97ddd79594948a8683d53162d883
Soomin <gil2093ab@gmail.com>
2025-02-25 13:03:15 +0900

    feat: add TutorialCubit and TutorialState for onboarding tutorial management

---
+++ lib/cubit/tutorial_cubit.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+part 'tutorial_state.dart';
+
+class TutorialCubit extends Cubit<TutorialState> {
+  TutorialCubit() : super(TutorialInitial());
+
+  final GlobalKey _enterRoomKey = GlobalKey();
+  final GlobalKey _verifyButtonKey = GlobalKey();
+
+  GlobalKey get enterRoomKey => _enterRoomKey;
+  GlobalKey get verifyButtonKey => _verifyButtonKey;
+}
---
+++ lib/cubit/tutorial_state.dart
@@ -0,0 +1,6 @@
+part of 'tutorial_cubit.dart';
+
+@immutable
+sealed class TutorialState {}
+
+final class TutorialInitial extends TutorialState {}
