486e7c960533610eb27feea9e31b1b81da0439b2
Soomin <gil2093ab@gmail.com>
2025-02-28 16:30:03 +0900

    feat: add chatButtonDetailKey to TutorialCubit for detailed chat button access

--- lib/cubit/tutorial_cubit.dart
+++ lib/cubit/tutorial_cubit.dart
@@ -18,9 +18,11 @@ class TutorialCubit extends Cubit<TutorialState> {
   final GlobalKey _verifyButtonKey = GlobalKey();
   final GlobalKey _chatRoomKey = GlobalKey();
   final GlobalKey _chatButtonKey = GlobalKey();
+  final GlobalKey _chatButtonDetailKey = GlobalKey();
 
   GlobalKey get enterRoomKey => _enterRoomKey;
   GlobalKey get chatRoomKey => _chatRoomKey;
   GlobalKey get verifyButtonKey => _verifyButtonKey;
   GlobalKey get chatButtonKey => _chatButtonKey;
+  GlobalKey get chatButtonDetailKey => _chatButtonDetailKey;
 }
