ad9f63cbb4e775acba3cad3e143a6c128f058099
zs0057 <soss4823@gmail.com>
2025-04-15 12:05:56 +0900

    feat: add hyper link

--- lib/widgets/chat_bubble.dart
+++ lib/widgets/chat_bubble.dart
@@ -10,6 +10,8 @@ import 'package:udaadaa/models/profile.dart';
 import 'package:udaadaa/utils/constant.dart';
 import 'package:udaadaa/view/chat/image_detail_view.dart';
 import 'package:udaadaa/view/chat/profile_view.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
 
 class ChatBubble extends StatelessWidget {
   const ChatBubble({
@@ -363,11 +365,31 @@ class ChatBubble extends StatelessWidget {
                     constraints: BoxConstraints(
                       maxWidth: MediaQuery.of(context).size.width * 0.6,
                     ),
-                    child: Text(
-                      message.text,
+                    child: Linkify(
+                      onOpen: (link) async {
+                        try {
+                          final Uri url = Uri.parse(link.url);
+                          if (await canLaunchUrl(url)) {
+                            await launchUrl(url,
+                                mode: LaunchMode.externalApplication);
+                          } else {
+                            throw Exception('Could not launch $url');
+                          }
+                        } catch (e) {
+                          debugPrint('Error launching URL: $e');
+                        }
+                      },
+                      text: message.text,
                       style: AppTextStyles.bodyLarge(
                         TextStyle(
                           color: AppColors.neutral[800],
+                        ),
+                      ),
+                      linkStyle: AppTextStyles.bodyLarge(
+                        TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                          decorationColor: Colors.blue,
                         ),
                       ),
                     ),
@@ -415,11 +437,31 @@ class ChatBubble extends StatelessWidget {
                 maxWidth: MediaQuery.of(context).size.width * 0.6,
               ),
               child: (message.medias == null || message.medias!.isEmpty
-                  ? Text(
-                      message.text,
+                  ? Linkify(
+                      onOpen: (link) async {
+                        try {
+                          final Uri url = Uri.parse(link.url);
+                          if (await canLaunchUrl(url)) {
+                            await launchUrl(url,
+                                mode: LaunchMode.externalApplication);
+                          } else {
+                            throw Exception('Could not launch $url');
+                          }
+                        } catch (e) {
+                          debugPrint('Error launching URL: $e');
+                        }
+                      },
+                      text: message.text,
                       style: AppTextStyles.bodyLarge(
                         TextStyle(
                           color: AppColors.neutral[800],
+                        ),
+                      ),
+                      linkStyle: AppTextStyles.bodyLarge(
+                        TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                          decorationColor: Colors.blue,
                         ),
                       ),
                     )
