1d951cdbc43e915712a7549e453f6773a4f83451
zs0057 <soss4823@gmail.com>
2025-05-17 18:47:12 +0900

    fix: pgsodium comment

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -2072,7 +2072,7 @@ SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 533, true);
 -- Name: key_key_id_seq; Type: SEQUENCE SET; Schema: pgsodium; Owner: supabase_admin
 --
 
-SELECT pg_catalog.setval('"pgsodium"."key_key_id_seq"', 1, false);
+-- SELECT pg_catalog.setval('"pgsodium"."key_key_id_seq"', 1, false);
 
 
 --
